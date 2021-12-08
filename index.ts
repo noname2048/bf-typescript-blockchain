15f0f8a86b8f1cac80eceff00dc69da3ceb0aaa5
noname2048 <sungwook.csw@gmail.com>
2021-12-08 23:47:02 +0900

    fix: fix scripts (prestart)

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-alert("hello")
+const name = "Nicolas",
+    age = 24,
+    gender = "mate";
+
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`)
+}
+
+sayHi(name, age, gender);
+
+export { name }
