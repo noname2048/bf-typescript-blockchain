28508ec56ab949f706417976f73745d5b007bd73
noname2048 <sungwook.csw@gmail.com>
2021-12-09 23:37:11 +0900

    fix: add types, fix typo error

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 const name = "Nicolas",
     age = 24,
-    gender = "mate";
+    gender = "male";
 
-const sayHi = (name, age, gender) => {
-    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`)
+const sayHi = (name: String, age: Number, gender: String): String => {
+    let str: String =`Hello ${name}, you are ${age}, you are a ${gender}`; 
+    console.log(str);
+    return str
 }
 
 sayHi(name, age, gender);
